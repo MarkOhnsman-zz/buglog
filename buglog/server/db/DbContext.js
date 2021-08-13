9829fd39afbb0d2e0ca5d500f036881907e95998
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-08-13 15:21:24 -0600

    sync

--- buglog/server/db/DbContext.js
+++ buglog/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
-import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
+import { Bug } from '../models/Bug'
+import { Value as ValueSchema } from '../models/Value'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
-  Account = mongoose.model('Account', AccountSchema);
+  Values = mongoose.model('Value', ValueSchema)
+  Bugs = mongoose.model('Bug', Bug)
+  Account = mongoose.model('Account', AccountSchema)
 }
 
 export const dbContext = new DbContext()
